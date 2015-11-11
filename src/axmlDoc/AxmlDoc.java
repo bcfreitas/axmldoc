36137ea943fa7284e6e9d679b0a88c841ae63dc9
Bruno Freitas <bcfreitas@gmailcom>
2015-11-11 11:45:36 -0300

    inserindo tipo de retorno

--- src/axmlDoc/AxmlDoc.java
+++ src/axmlDoc/AxmlDoc.java
@@ -1,4 +1,5 @@
 package axmlDoc;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,96 +21,107 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class AxmlDoc {
-		private Document xml;
-		private Document schema;
-		
-		public AxmlDoc(File xml, File schema){	
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder;
-			Document doc = null, doc2 = null;
-			try {
-				dBuilder = dbFactory.newDocumentBuilder();
-				doc = dBuilder.parse(xml);
-				doc2 = dBuilder.parse(schema);
+	private Document xml;
+	private Document schema;
 
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SAXException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		 
-			this.setXml(doc);
-			this.setSchema(doc2);
+	public enum ReturnType {
+		STRING, XMLTREE
+	};
+
+	public AxmlDoc(File xml, File schema) {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		Document doc = null, doc2 = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(xml);
+			doc2 = dBuilder.parse(schema);
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		public Document getXml() {
-			return xml;
+		this.setXml(doc);
+		this.setSchema(doc2);
+	}
+
+	public Document getXml() {
+		return xml;
+	}
+
+	public void setXml(Document xml) {
+		this.xml = xml;
+	}
+
+	public Document getSchema() {
+		return schema;
+	}
+
+	public void setSchema(Document schema) {
+		this.schema = schema;
+	}
+
+	public List<Node> verificarChamadasRemotas() {
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		String busca = "//axml:call";
+
+		// read a string value
+		// String email = xPath.compile(busca).evaluate(this.getXml());
+
+		// read an xml node using xpath
+		// Node node = (Node) xPath.compile(expression).evaluate(xmlDocument,
+		// XPathConstants.NODE);
+
+		// read a nodelist using xpath
+		// NodeList nodeList = (NodeList)
+		// xPath.compile(expression).evaluate(xmlDocument,
+		// XPathConstants.NODESET);
+
+		// Busca todas os nos com chamadas a serviço remoto
+		NodeList remoteCalls = this.getXml().getElementsByTagName("axml:call");
+
+		List<Node> remoteCallsParents = new ArrayList<Node>();
+		for (int n = remoteCalls.getLength(); n > 0; n--) {
+			Node noPai = remoteCalls.item(n - 1).getParentNode();
+			remoteCallsParents.add(noPai);
 		}
 
-		public void setXml(Document xml) {
-			this.xml = xml;
-		}
+		return remoteCallsParents;
 
-		public Document getSchema() {
-			return schema;
-		}
+	}
 
-		public void setSchema(Document schema) {
-			this.schema = schema;
-		}
-		
-		public List<Node> verificarChamadasRemotas(){
-			XPath xPath =  XPathFactory.newInstance().newXPath();
-			String busca = "//axml:call";
-			 
-			//read a string value
-			//	String email = xPath.compile(busca).evaluate(this.getXml());
-			 
-			//read an xml node using xpath
-			//Node node = (Node) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODE);
-			 
-			//read a nodelist using xpath
-			//NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-			
-			//Busca todas os nos com chamadas a serviço remoto
-			NodeList remoteCalls = this.getXml().getElementsByTagName("axml:call");
-			
-			List<Node> remoteCallsParents = new ArrayList<Node>();
-			for(int n = remoteCalls.getLength(); n>0; n-- ){
-				System.out.println("aaa " + remoteCalls.item(n-1).getParentNode());
-				Node noPai = remoteCalls.item(n-1).getParentNode();
-				remoteCallsParents.add(noPai);
-			}
-			
-			return remoteCallsParents;
+	public Node materializarElemento(Node no, ReturnType returnType) throws IOException,
+			SAXException, ParserConfigurationException {
+		Element elemento = (Element) no;
+		// captura endereço do webservice
+		Element chamadaRemota = (Element) elemento.getElementsByTagName("axml:call").item(0);
+		URL enderecoRemoto = new URL(chamadaRemota.getAttribute("service"));
 
-		}
+		// estabelece conexao
+		HttpURLConnection conexao = (HttpURLConnection) enderecoRemoto.openConnection();
 
-		public Node materializarElemento(Node no) throws IOException, SAXException, ParserConfigurationException{
-			Element elemento = (Element)no;
-			//captura endereço do webservice
-			Element chamadaRemota = (Element)elemento.getElementsByTagName("axml:call").item(0);
-			URL enderecoRemoto = new URL(chamadaRemota.getAttribute("service"));
-			System.out.println(enderecoRemoto.toString());
-			
-			//estabelece conexao
-			HttpURLConnection conexao = (HttpURLConnection) enderecoRemoto.openConnection();
-			
-			//recebe dados
-			InputStream content = conexao.getInputStream();
-			
-			//cria documento XML com dados recebidos
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document retornoXML = dBuilder.parse(content);
-			
-			//retorna objeto Node com dados obtidos
+		// recebe dados
+		InputStream content = conexao.getInputStream();
+
+		// cria documento XML com dados recebidos
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document retornoXML = dBuilder.parse(content);
+
+		// retorna objeto Node com dados obtidos
+		if (returnType.equals(ReturnType.STRING)) {
 			no.setTextContent(retornoXML.getDocumentElement().getFirstChild().getTextContent());
-			return no;
+		} else if(returnType.equals(ReturnType.XMLTREE)){
+			no.appendChild(retornoXML.getDocumentElement());
 		}
+
+		return no;
+	}
 }
--- src/desktop/client/ReadXMLFile.java
+++ src/desktop/client/ReadXMLFile.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -12,50 +16,91 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import axmlDoc.AxmlDoc;
- 
+import axmlDoc.AxmlDoc.ReturnType;
+
 public class ReadXMLFile {
- 
-  public static void main(String argv[]) {
- 
-    try {
- 
-	File fXmlFile = new File("livro.xml");
-	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	Document doc = dBuilder.parse(fXmlFile);
-	
-	//optional, but recommended
-	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-	//doc.getDocumentElement().normalize();
- 
-	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
- 
-	NodeList nList = doc.getElementsByTagName("livro");
- 
-	System.out.println("----------------------------");
- 
-	for (int temp = 0; temp < nList.getLength(); temp++) {
- 
-		Node nNode = nList.item(temp);
- 
-		System.out.println("\nCurrent Element :" + nNode.getNodeName());
- 
-		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
- 
-			Element eElement = (Element) nNode;
- 
-			System.out.println("Nome do livro: " + eElement.getAttribute("titulo"));
+
+	public static void main(String argv[]) {
+
+		try {
+
+			File fXmlFile = new File("livro.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("livro");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+
+					// Exibe nome do Livro
+					System.out.println("Nome do livro: " + eElement.getAttribute("titulo"));
+
+					// Exibe lista de capítulos do livro
+					NodeList capitulos = nNode.getChildNodes();
+					for (int c = 0; c < capitulos.getLength(); c++) {
+						Node capituloNode = capitulos.item(c);
+						if (capituloNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element capitulo = (Element) capituloNode;
+							System.out.println("Capitulo " + capitulo.getAttribute("numero") + ": "
+									+ capitulo.getAttribute("titulo"));
+						}
+					}
+				}
+
+				// Iniciando simulação de uso do usuário
+
+				XPathFactory xPathFactory = XPathFactory.newInstance();
+				XPath xPath = xPathFactory.newXPath();
+				XPathExpression pathExpression;
+				Node node;
+				Node remoteCall;
+				NodeList remoteCallList;
+				AxmlDoc axmlDoc = new AxmlDoc(fXmlFile, fXmlFile);
+
+				// read a string value
+				// System.out.println(xPath.compile(pathExpression).evaluate(doc));
+
+				// Usuário escolhe o capítulo 1 para leitura:
+				pathExpression = xPath.compile("/livro/capitulo[1]");
+				node = (Node) pathExpression.evaluate(doc, XPathConstants.NODE);
+				System.out.println("");
+				System.out.println("---> Usuário escolhe o capítulo 1 para leitura:");
+				System.out.println(node.getTextContent());
+				System.out.println("");
+
+				// Usuário escolhe o capítulo 2 para leitura:
+				pathExpression = xPath.compile("/livro/capitulo[2]");
+				node = (Node) pathExpression.evaluate(doc, XPathConstants.NODE);
+				System.out.println("");
+				System.out.println("---> Usuário escolhe o capítulo 2 para leitura:");
+
+				pathExpression = xPath.compile("/livro/capitulo[2]/*[name()='axml:call']");
+				remoteCallList = (NodeList) pathExpression.evaluate(doc, XPathConstants.NODESET);
+				if (remoteCallList != null && remoteCallList.getLength() == 0) {
+					System.out.println(node.getTextContent());
+				} else {
+					for (int remoteCallCount = 1; remoteCallCount <= remoteCallList.getLength(); remoteCallCount++) {
+						pathExpression = xPath.compile("/livro/capitulo[2]/axml:call["
+								+ remoteCallCount + "]");
+						remoteCall = (Node) pathExpression.evaluate(doc, XPathConstants.NODE);
+						System.out.println(axmlDoc.materializarElemento(node, ReturnType.STRING).getTextContent());
+					}
+
+				}
+				System.out.println("");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		AxmlDoc axmlDoc = new AxmlDoc(fXmlFile, fXmlFile);
-		System.out.println(axmlDoc.verificarChamadasRemotas().toString());
-		
-		List<Node> nosComCR = axmlDoc.verificarChamadasRemotas();
-		System.out.println(axmlDoc.materializarElemento(nosComCR.get(0)).getTextContent());
 	}
-    } catch (Exception e) {
-	e.printStackTrace();
-    }
-  }
- 
+
 }
